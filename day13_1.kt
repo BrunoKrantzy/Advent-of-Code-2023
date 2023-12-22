2fb53b68ccfc5ef146adfc03c463147ac39f47b7
Bruno DENIS <bruno.denis@gmail.com>
2023-12-22 19:27:17 +0100

    Add files via upload

--- day13_1.kt
+++ day13_1.kt
@@ -2,17 +2,17 @@ import kotlin.math.abs
 
 fun main() {
 
-    var inLines: List<String>
+    val inLines: List<String>
 
-    val bTest = true
+    val bTest = false
     if (bTest) {
         inLines = readInput("test13_1")
     } else {
         inLines = readInput("input13_1")
     }
 
-    var lstPatterns = mutableListOf<MutableList<String>>()
-    var lstTemp = mutableListOf<String>()
+    val lstPatterns = mutableListOf<MutableList<String>>()
+    val lstTemp = mutableListOf<String>()
     inLines.forEach {
         if (it.isEmpty()) {
             lstPatterns.add(lstTemp.toMutableList())
@@ -28,7 +28,7 @@ fun main() {
     var nbL = 0
     var nbC = 0
 
-    // rotation du plateau permettant la future rech/rep.
+    // rotation du plateau pour traiter le vertical
     fun turnPlato(platoIn: MutableList<String>) : MutableList<String> {
         var platoOut = mutableListOf<String>()
         val nl = platoIn.size
@@ -46,8 +46,9 @@ fun main() {
 
     data class Result(val res:Boolean, val goodLines:Int)
 
-    fun testSuite(pAt:MutableList<String>, ligBase:Int, ligLast:Int) : Result {
-        var goodLi = abs(ligLast - ligBase) + 1
+    fun testSuitePrem(pAt:MutableList<String>, ligBase:Int, ligLast:Int, ligneDep: Int) : Result {
+        var ligDep = ligneDep
+        var goodLi = 2 + abs(ligLast - ligDep)
         var res = Result(false, 0)
         var nL = 1 + abs((ligLast - ligBase))
         var ligMatch = ligLast
@@ -70,6 +71,32 @@ fun main() {
         return res
     }
 
+    fun testSuiteDer(pAt:MutableList<String>, ligBase:Int, ligLast:Int, ligneDep: Int) : Result {
+        var ligDep = ligneDep
+        var goodLi = abs(ligLast - ligDep)
+        var res = Result(false, 0)
+        var nL = 1 + abs((ligLast - ligBase))
+        var ligMatch = ligLast
+        var ligFirst = ligBase
+        var test = true
+        for (i in 1 .. nL/2) {
+            val l1 = pAt[ligFirst]
+            val l2 = pAt[ligMatch]
+            if (l1 != l2) {
+                test = false
+                goodLi = 0
+                break
+            }
+            else {
+                ligFirst++
+                ligMatch--
+            }
+        }
+        res = Result(test, goodLi)
+        return res
+    }
+
+
     var lstValPosH = mutableListOf<Int>()
     var lstValPosV = mutableListOf<Int>()
 
@@ -78,24 +105,25 @@ fun main() {
         var pV = turnPlato(pH)
 
         // traitement pH
-        nbL = pH.size
-        nbC = pH[0].length
         var patHgood = false
 
         // première ligne valide ?
         // on cherche en partant de la dernière ligne celle qui match avec la 1ere
+        nbL = pH.size
         var nbRowsTemp1 = 0
         var ligBase = 0
         var firstLig = pH[ligBase]
+        var ligneDep = 0
 
-        for (ll in 1 .. nbL-2) {
-            for (ligSuiv in nbL-ll downTo ligBase + 1) {
+        for (ll in 0 .. 0) {
+            for (ligSuiv in nbL-1 downTo ligBase + 1) {
                 val str = pH[ligSuiv]
                 if (firstLig == str && (1 + (ligSuiv - ligBase)) % 2 == 0) {
+                    ligneDep = 0
                     // vérifier le matching des lignes suivantes
-                    val result = testSuite(pH, ligBase + 1, ligSuiv - 1)
+                    val result = testSuitePrem(pH, ligBase + 1, ligSuiv - 1, ligneDep)
                     if (result.res == true) {
-                        nbRowsTemp1 = maxOf(nbRowsTemp1, result.goodLines)
+                        nbRowsTemp1 = maxOf(nbRowsTemp1, ligneDep + result.goodLines / 2)
                         patHgood = true
                         break
                     }
@@ -109,16 +137,19 @@ fun main() {
         ligBase = pH.size - 1
         firstLig = pH[ligBase]
 
-        for (ll in 0 .. nbL-2) {
-            for (ligSuiv in ll until pH.size-1) {
-                val str = pH[ligSuiv]
-                if (firstLig == str && (1 + (ligBase - ligSuiv)) % 2 == 0) {
-                    // vérifier le matching des lignes suivantes
-                    val result = testSuite(pH, ligSuiv + 1, ligBase - 1)
-                    if (result.res == true) {
-                        nbRowsTemp2 = maxOf(nbRowsTemp2, result.goodLines)
-                        patHgood = true
-                        break
+        if (!patHgood) {
+            for (ll in 0 .. 0) {
+                for (ligSuiv in pH.size-1 downTo 1) {
+                    val str = pH[ligSuiv]
+                    if (firstLig == str && (1 + (ligBase - ligSuiv)) % 2 == 0) {
+                        ligneDep = ligSuiv
+                        // vérifier le matching des lignes suivantes
+                        val result = testSuiteDer(pH, ligSuiv + 1, ligBase - 1, ligneDep)
+                        if (result.res == true) {
+                            nbRowsTemp2 = maxOf(nbRowsTemp1, ligneDep + ((2 + result.goodLines) / 2))
+                            patHgood = true
+                            break
+                        }
                     }
                 }
             }
@@ -127,54 +158,54 @@ fun main() {
         nbRows += maxOf(nbRowsTemp1, nbRowsTemp2)
 
 
-
         // traitement pV
+
+        // première ligne valide ?
+        // on cherche en partant de la dernière ligne celle qui match avec la 1ere
         nbL = pV.size
         var patVgood = false
         var nbColsTemp1 = 0
         ligBase = 0
         firstLig = pV[ligBase]
 
-        for (ll in 1 .. nbL-2) {
-            for (ligSuiv in nbL-ll downTo ligBase + 1) {
+        for (ll in 0 .. 0) {
+            for (ligSuiv in nbL-1 downTo ligBase + 1) {
                 val str = pV[ligSuiv]
                 if (firstLig == str && (1 + (ligSuiv - ligBase)) % 2 == 0) {
+                    ligneDep = 0
                     // vérifier le matching des lignes suivantes
-                    val result = testSuite(pV, ligBase + 1, ligSuiv - 1)
+                    val result = testSuitePrem(pV, ligBase + 1, ligSuiv - 1, ligneDep)
                     if (result.res == true) {
-                        nbColsTemp1 = 1 + ((2 + result.goodLines) / 2)
+                        nbColsTemp1 = maxOf(nbColsTemp1, ligneDep + result.goodLines / 2)
                         patVgood = true
                         break
                     }
                 }
             }
         }
-
 
         // dernière ligne valide ?
         var nbColsTemp2 = 0
         ligBase = pV.size - 1
         firstLig = pV[ligBase]
 
-        for (ll in 0 .. nbL-2) {
-            for (ligSuiv in ll until pV.size-1) {
-                val str = pV[ligSuiv]
-                if (firstLig == str && (1 + (ligBase - ligSuiv)) % 2 == 0) {
-                    // vérifier le matching des lignes suivantes
-                    val result = testSuite(pV, ligSuiv + 1, ligBase - 1)
-                    if (result.res == true) {
-                        nbColsTemp2 = maxOf(nbColsTemp1, 1 + (2 + result.goodLines) / 2)
-                        patVgood = true
-                        break
+        if (!patVgood) {
+            for (ll in 0 .. 0) {
+                for (ligSuiv in pV.size-1 downTo 1) {
+                    val str = pV[ligSuiv]
+                    if (firstLig == str && (1 + (ligBase - ligSuiv)) % 2 == 0) {
+                        ligneDep = ligSuiv
+                        // vérifier le matching des lignes suivantes
+                        val result = testSuiteDer(pV, ligSuiv + 1, ligBase - 1, ligneDep)
+                        if (result.res == true) {
+                            nbColsTemp2 = maxOf(nbColsTemp1, ligneDep + ((2 + result.goodLines) / 2))
+                            patVgood = true
+                            break
+                        }
                     }
                 }
             }
         }
-
-        if (patVgood && patHgood)
-            println("ERR 1")
-        if (!patVgood && !patHgood)
-            println("ERR 2")
 
         nbCols += maxOf(nbColsTemp1, nbColsTemp2)
 
